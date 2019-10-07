44dbfa612f27a10648220b5369e7bf8417f69f5c
Anton Kosiakov <anton.kosyakov@typefox.io>
2019-10-07 13:04:36 +0200

    initial solution-4

---
+++ theia-training/src/browser/jsonschema-form-open-handler.ts
@@ -0,0 +1,34 @@
+import { WidgetOpenHandler } from "@theia/core/lib/browser";
+import { JsonschemaFormWidget, JsonschemaFormWidgetOptions } from "./jsonschema-form-widget";
+import URI from "@theia/core/lib/common/uri";
+import { injectable, inject } from "inversify";
+import { EditorManager } from "@theia/editor/lib/browser";
+import { JsonschemaFormPreferences } from "./jsonschema-form-preferences";
+
+@injectable()
+export class JsonschemaFormOpenHandler extends WidgetOpenHandler<JsonschemaFormWidget> {
+    
+    readonly id = JsonschemaFormWidget.id;
+    readonly label = "Form";
+
+    @inject(EditorManager)
+    protected readonly editorManager: EditorManager;
+
+    @inject(JsonschemaFormPreferences)
+    protected readonly preferences: JsonschemaFormPreferences;
+
+    canHandle(uri: URI): number {
+        if (uri.path.ext !== '.json') {
+            return 0;
+        }
+        if (uri.path.name.endsWith(this.preferences["jsonschema-form.dataSuffix"])) {
+            return this.editorManager.canHandle(uri) * 2;    
+        }
+        return this.editorManager.canHandle(uri) / 2;
+    }
+
+    protected createWidgetOptions(uri: URI): JsonschemaFormWidgetOptions {
+        return { uri: uri.withoutFragment().toString() };
+    }
+    
+}
--- theia-training/src/browser/theia-training-frontend-module.ts
+++ theia-training/src/browser/theia-training-frontend-module.ts
@@ -1,21 +1,40 @@
 import '../../src/browser/style/index.css';
 
-import { ContainerModule } from 'inversify';
-import { FileListViewContribution } from './file-list-view-contribution';
-import { bindViewContribution, WidgetFactory, FrontendApplicationContribution, WebSocketConnectionProvider } from '@theia/core/lib/browser';
-import { FileListWidget } from './file-list-widget';
-import { FileListService, fileListPath } from '../common/file-list-protocol';
+import { ContainerModule } from "inversify";
+import { OpenHandler, WidgetFactory } from "@theia/core/lib/browser";
+import { ThemeService } from "@theia/core/lib/browser/theming";
+import { JsonschemaFormWidget, JsonschemaFormWidgetOptions } from './jsonschema-form-widget';
+import { JsonschemaFormOpenHandler } from './jsonschema-form-open-handler';
+import { bindJsonschemaFormPreferences } from "./jsonschema-form-preferences";
+
+const bootstrapLink = document.createElement("link");
+bootstrapLink.rel = 'stylesheet';
+document.head.appendChild(bootstrapLink);
+
+function updateTheme(newTheme: string): void {
+    const theme = bootstrapLink.getAttribute('theme');
+    if (theme === newTheme) {
+        return;
+    }
+    bootstrapLink.setAttribute('theme', newTheme);
+    bootstrapLink.href = newTheme === 'dark' ?
+        '//cdnjs.cloudflare.com/ajax/libs/bootswatch/3.3.6/darkly/bootstrap.min.css' :
+        '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css';
+}
+updateTheme(ThemeService.get().getCurrentTheme().id);
+ThemeService.get().onThemeChange(({ newTheme }) => updateTheme(newTheme.id));
 
 export default new ContainerModule(bind => {
-    bind(FileListWidget).toSelf();
-    bind(WidgetFactory).toDynamicValue(context => ({
-        id: FileListWidget.ID,
-        createWidget: () => context.container.get(FileListWidget)
+    bind(OpenHandler).to(JsonschemaFormOpenHandler).inSingletonScope();
+    bind(WidgetFactory).toDynamicValue(({ container }) => ({
+        id: JsonschemaFormWidget.id,
+        createWidget: (options: JsonschemaFormWidgetOptions) => {
+            const child = container.createChild();
+            child.bind(JsonschemaFormWidgetOptions).toConstantValue(options);
+            child.bind(JsonschemaFormWidget).toSelf();
+            return child.get(JsonschemaFormWidget);
+        }
     }));
-    bindViewContribution(bind, FileListViewContribution);
-    bind(FrontendApplicationContribution).toService(FileListViewContribution);
 
-    bind(FileListService).toDynamicValue(ctx =>
-        WebSocketConnectionProvider.createProxy(ctx.container, fileListPath)
-    ).inSingletonScope();
+    bindJsonschemaFormPreferences(bind);
 });
