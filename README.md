29ca818e3165115fe4328c5deb88d70e53342b72
Alex Tugarev <alex.tugarev@typefox.io>
2020-02-28 06:42:32 +0000

    Update: remove obsolete theming part

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ In this exercise, you learn:
 - how to customize the default layout;
 - how to customize the menu bar;
 - how to remove functionality;
-- how to customize theming.
 
 ### Task 1: Customize the default layout
 - Customize the default layout in `TheiaTrainingFrontendContribution.initializeLayout`.
@@ -18,11 +17,6 @@ In this exercise, you learn:
 Use `MenuModelRegistry.unregisterMenuAction` with the last segment of `TerminalMenus.TERMINAL` and `MAIN_MENU_BAR`.
 - Add logo icon. In `index.css` set `background-image` of `theia-icon` class to `url(gitpod-logo.svg)`.
 
-### Task 3: Add a new theme
-- In `theia-training-frontend-module.ts` set `my-dark` as a current theme.
-- Modify variables-my-dark.useable.css to set `--theia-ui-font-color1` variable as `#6688CC`.
-- Use `Change Color Theme` command to switch between themes and see how text color is changed in the explorer.
-
 ## Bonus
 
 ### Task 4: More customizations to the default layout
@@ -33,8 +27,3 @@ Use `MenuModelRegistry.unregisterMenuAction` with the last segment of `TerminalM
 
 ### Task 5: Remove `Call Hierarchy` contribution
 - Rebind `CallHierarchyContribution` to a constant object stubbing commands, menus and keybidnings registration.
-
-### Task 6: Customize the editor theme
-- Change `my-dark` theme to use `myEditorTheme` as an editor theme in `theia-training-frontend-module.ts`
-- Configure `foreground` as `#22aa44` (green) for `strings` tokens in `string.quoted.single`, `string.quoted.double` and `string.quoted.triple` scopes.
-- See https://code.visualstudio.com/api/extension-capabilities/theming#color-theme to learn more about tokens, scopes and token colors.
--- theia-training/src/browser/theia-training-frontend-module.ts
+++ theia-training/src/browser/theia-training-frontend-module.ts
@@ -1,41 +1,14 @@
 import '../../src/browser/style/index.css';
 
-const myDarkCss = require('../../src/browser/style/variables-my-dark.useable.css');
-
-
 import { ContainerModule } from 'inversify';
 import { TheiaTrainingFrontendContribution } from './theia-training-frontend-contribution';
 import { FrontendApplicationContribution } from '@theia/core/lib/browser';
 import { MenuContribution } from '@theia/core';
-import { ThemeService, BuiltinThemeProvider } from '@theia/core/lib/browser/theming';
-
-// BONUS: Use myEditorTheme as an editor theme for `my-dark` theme. Change my.color-theme.json to highlight strings in green.
-// See https://code.visualstudio.com/api/extension-capabilities/theming#color-theme to learn more about tokens, scopes and token colors.
-// import { MonacoThemeRegistry } from '@theia/monaco/lib/browser/textmate/monaco-theme-registry';
-// const myEditorTheme = MonacoThemeRegistry.SINGLETON.register(
-//    require('../../src/browser/data/my.color-theme.json'), {}, 'my-dark', 'vs-dark').name!;
 
 export default new ContainerModule((bind, unbind, isBound, rebind) => {
     bind(TheiaTrainingFrontendContribution).toSelf().inSingletonScope();
     bind(MenuContribution).toService(TheiaTrainingFrontendContribution);
     bind(FrontendApplicationContribution).toService(TheiaTrainingFrontendContribution);
-
-    const themeService = ThemeService.get();
-    themeService.register({
-        id: 'my-dark',
-        label: 'My Dark Theme',
-        description: 'Bright fonts on dark backgrounds.',
-        editorTheme: 'dark-plus', // loaded in /packages/monaco/src/browser/textmate/monaco-theme-registry.ts
-        activate(): void {
-            BuiltinThemeProvider.darkCss.use();
-            myDarkCss.use();
-        },
-        deactivate(): void {
-            myDarkCss.unuse();
-            BuiltinThemeProvider.darkCss.unuse();
-        }
-    });
-    // TODO: set my-dark as a current theme
 
     // BONUS: remove `Call Hierarch` view contribution
 });
